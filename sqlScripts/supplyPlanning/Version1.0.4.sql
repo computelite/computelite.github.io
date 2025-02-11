4c1fc22acff2d4619edf16a0e464effb6bccd653
airen1986 <45193110+airen1986@users.noreply.github.com>
2025-02-11 11:56:35 +0530

    update sql

--- sqlScripts/supplyPlanning/Version1.0.4.sql
+++ sqlScripts/supplyPlanning/Version1.0.4.sql
@@ -1,7 +1,10 @@
-CREATE TABLE S_Notebook (
+CREATE TABLE IF NOT EXISTS S_Notebook (
 	CellId	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
 	CellContent	VARCHAR,
 	CreationDate	VARCHAR DEFAULT (datetime('now','localtime'))
 );
 
-INSERT INTO S_TableGroup (GroupName, TableName, TableDisplayName, TableType, ColumnOrder, Table_Status, Freeze_Col_Num) VALUES('All Other','S_Notebook','Notebook','Input',null,'Active',NULL);
+DELETE FROM S_TableGroup WHERE TableName = 'S_Notebook';
+
+INSERT INTO S_TableGroup (GroupName, TableName, TableDisplayName, TableType, ColumnOrder, Table_Status, Freeze_Col_Num) 
+VALUES('All Other','S_Notebook','Notebook','Input',null,'Active',NULL);
